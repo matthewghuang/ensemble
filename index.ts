188098969c8af1b954ade77e1ff10e7121b8229f
matthewghuang <matthewhhuang@gmail.com>
2020-12-10 13:20:58 -0800

    socket.io server & client

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world")
+import { Server, Socket } from "socket.io"
+
+// setup socket.io server on port 3000
+const io = new Server(3000, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket: Socket) => {
+    console.log("user connected")
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+    })
+})
