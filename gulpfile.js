b1bd6e95ebb85c9922007c0e62d9d7c97ae63602
matthewghuang <matthewhhuang@gmail.com>
2020-12-15 08:23:33 -0800

    dont ignore gulpfile in gitignore...

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require("gulp")
+const ts = require("gulp-typescript")
+const htmlmin = require("gulp-htmlmin")
+const terser = require("gulp-terser")
+const pug = require("gulp-pug")
+
+const compiler_options = {
+	target: "ES6",
+	strict: true,
+	esModuleInterop: true,
+	skipLibCheck: true,
+	downlevelIteration: true
+}
+
+const compile_server = () => {
+	return gulp.src([
+			"src/server/index.ts",
+			"src/common/*.ts"
+		], { base: "src" })
+		.pipe(ts(compiler_options))
+		.pipe(gulp.dest("out"))
+
+}
+
+const compile_web = () => {
+	return gulp.src([
+			"src/web/client.ts",
+			"src/common/*.ts"
+		], { base: "src" })
+		.pipe(ts({
+			...compiler_options,
+			module: "system",
+			moduleResolution: "node",
+			outFile: "bundle.js"
+		}))
+		.pipe(terser())
+		.pipe(gulp.dest("web"))
+}
+
+const build_html = () => {
+	return gulp.src("src/web/*.pug")
+		.pipe(pug())
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest("web"))
+}
+
+gulp.task("web", gulp.series(compile_web, build_html))
+gulp.task("web-dev", () => {
+	gulp.watch("src/web/*.pug", build_html)
+	gulp.watch("src/web/*.ts", compile_web)
+})
+
+gulp.task("server", gulp.series(compile_server))
