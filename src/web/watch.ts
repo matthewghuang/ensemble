204c8b7a18939d17cf5ae887d8413e2e4dc1613d
matthewghuang <matthewhhuang@gmail.com>
2020-12-16 21:41:55 -0800

    use plyr instead of html5 video

--- src/web/watch.ts
+++ src/web/watch.ts
@@ -2,8 +2,8 @@ import { io } from "socket.io-client"
 import Update from "../common/update"
 import Events from "../common/events"
 import * as flag from "../common/flag"
+import Plyr from "plyr"
 
-const video_element = document.getElementById("video") as HTMLVideoElement
 const members_ul = document.getElementById("members_ul") as HTMLUListElement
 
 const socket = io("http://localhost:3000")
@@ -17,18 +17,26 @@ enum ClientFlags {
 let state: number = ClientFlags.NONE
 let members: Array<[string, string]> = []
 let your_name: string | undefined
+let player: Plyr
 
 const is_host = (your_name: string, members: Array<[string,string]>): boolean => {
 	return members[0][1] == your_name
 }
 
+const initialize_player = () => {
+	player = new Plyr("#video")
+}
+
 const update_member_list = (members: Array<[string, string]>) => {
 	members_ul.innerHTML = ""
 
-	members.forEach(member => {
+	members.forEach((member, idx) => {
 		const [, name] = member
 		const message_li = document.createElement("li")
-		message_li.innerHTML = name
+		if (idx == 0)
+			message_li.innerHTML = name + " (host)"
+		else
+			message_li.innerHTML = name
 		members_ul.append(message_li)
 	})
 }
@@ -67,30 +75,41 @@ socket.on(Events.UPDATE_MEMBERS, (updated_members: Array<[string, string]>) => {
 socket.on(Events.UPDATE_CLIENT, (update: Update) => {
 	const { src, time, paused } = update
 
-	if (src != undefined && src != video_element.src)
-		video_element.src = src
+	/* if (src != undefined && src != player.source.sources[0].src) {
+		player.source = {
+			type: "video",
+			sources: [
+				{
+					src: src,
+					type: "video/mp4"
+				}
+			]
+		}
+	} */
 
-	if (time != undefined && Math.abs(time - video_element.currentTime) > 1)
-		video_element.currentTime = time
+	if (time != undefined && Math.abs(time - player.currentTime) > 1)
+		player.currentTime = time
 
-	if (paused != undefined && paused != video_element.paused)
-		paused ? video_element.pause() : video_element.play()
+	if (paused != undefined && paused != player.paused)
+		paused ? player.pause() : player.play()
 })
 
 setInterval(() => {
 	if (flag.has_flag(state, ClientFlags.HOST)) {
 		update_server({
-			src: video_element.src,
-			time: video_element.currentTime,
-			paused: video_element.paused
+			src: "http://techslides.com/demos/sample-videos/small.mp4",
+			time: player.currentTime,
+			paused: player.paused
 		})
 	}
 }, 1000)
+
+initialize_player()
 
 setTimeout(() => {
 	join_room()
 }, 1000)
 
-video_element.onseeked = () => update_server({ time: video_element.currentTime })
-video_element.onplay = () => update_server({ paused: false })
-video_element.onpause = () => update_server({ paused: true })
+player.on("seeked", () => update_server({ time: player.currentTime }))
+player.on("play", () => update_server({ paused: false }))
+player.on("pause", () => update_server({ paused: true }))
